6380254d03bca6f0d6fbb4935fbfe570c62c9125
Richard Sebold <138056060+richardsebold@users.noreply.github.com>
2025-12-14 15:29:30 -0300

    adicionar suporte a modo escuro e implementar filtragem por categoria nos vídeos

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ async function buscarEMostrarVideos() {
                         <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
                 `;
@@ -38,7 +39,9 @@ function filtrarPesquisa() {
 
   if (barraDePesquisa.value != "") {
     for (let video of videos) {
-      let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+      let titulo = video
+        .querySelector(".titulo-video")
+        .textContent.toLowerCase();
       let valorFiltro = barraDePesquisa.value.toLowerCase();
 
       if (!titulo.includes(valorFiltro)) {
@@ -53,3 +56,34 @@ function filtrarPesquisa() {
     }
   }
 }
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+  let nomeCategoria = botao.getAttribute("name");
+  botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+  const videos = document.querySelectorAll(".videos__item");
+  for (let video of videos) {
+    let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+    let valorFiltro = filtro.toLowerCase();
+    if (!categoria.includes(valorFiltro) && valorFiltro != "tudo") {
+      video.style.display = "none"
+    } else {
+      video.style.display = "block"
+    }
+  }
+}
+
+const botaoTrocaTema = document.querySelector(".cabecalho__switch-input");
+
+function mudaTema() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+  localStorage.setItem("theme", body.classList.contains("dark-mode") ? "dark" : "light");
+}
+
+botaoTrocaTema.addEventListener("change", mudaTema);
+
