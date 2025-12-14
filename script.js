2f8aa699eb5584baf82228d25c16e74c61ad4c82
Richard Sebold <138056060+richardsebold@users.noreply.github.com>
2025-12-14 01:26:02 -0300

    refatorar estrutura do HTML e melhorar a função de busca de vídeos

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const containerVideos = document.querySelector(".videos__container");
 
+async function buscarEMostrarVideos() {
+  try {
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
 
-async function buscarEMostrarVideos(){
-    try{
-        const busca = await fetch("http://localhost:3000/videos");
-        const videos = await busca.json();
-
-            videos.forEach((video)=> {
-                if(video.categoria == ""){
-                    throw new Error('Vídeo não tem categoria');
-                }
-                containerVideos.innerHTML += `
+    videos.forEach((video) => {
+      if (video.categoria == "") {
+        throw new Error("Vídeo não tem categoria");
+      }
+      containerVideos.innerHTML += `
                 <li class="videos__item">
                     <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                     <div class="descricao-video">
@@ -20,12 +19,37 @@ async function buscarEMostrarVideos(){
                     </div>
                 </li>
                 `;
-            })
-    } catch(error){
-        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
-    }
+    });
+  } catch (error) {
+    containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`;
+  } finally {
+    console.log("Processamento concluído.");
+  }
 }
-
 
 buscarEMostrarVideos();
 
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+  const videos = containerVideos.querySelectorAll(".videos__item");
+
+  if (barraDePesquisa.value != "") {
+    for (let video of videos) {
+      let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+      let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+      if (!titulo.includes(valorFiltro)) {
+        video.style.display = "none";
+      } else {
+        video.style.display = "block";
+      }
+    }
+  } else {
+    for (let video of videos) {
+      video.style.display = "block";
+    }
+  }
+}
